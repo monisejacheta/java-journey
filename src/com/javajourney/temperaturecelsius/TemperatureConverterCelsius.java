1049372c68f48b94d617a33e13e2d52f903f6f4d
Monise Jacheta <monisel.jacheta@gmail.com>
2025-09-05 14:04:44 -0300

    Add files via upload
    [en] Adding day of the week challenge returning number
    [pt] Adicionando desafio de dia da semana retornando número

---
+++ src/com/javajourney/temperaturecelsius/TemperatureConverterCelsius.java
@@ -0,0 +1,26 @@
+package com.javajourney.temperaturecelsius;
+
+import java.util.Scanner;
+
+public class TemperatureConverterCelsius {
+
+  public static void main(String[] args) {
+     /*
+       [en] Temperature converter from Celsius to Fahrenheit
+       [pt] Conversor de temperatura de Celsius para Fahrenheit
+       */
+    Scanner scannerSystem = new Scanner(System.in);
+
+    double celciusTemperature;
+    System.out.println("Enter the Celsius temperature here: ");
+    celciusTemperature = scannerSystem.nextDouble();
+
+    double temperatureFinal;
+    temperatureFinal = (celciusTemperature * 1.8) + 32;
+    System.out.println("The temperature is: " + String.format("%.2f", temperatureFinal) + "°F");
+
+
+    scannerSystem.close();
+  }
+
+}
